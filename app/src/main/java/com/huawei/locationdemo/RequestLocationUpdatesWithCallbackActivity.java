9e7df7c7671501b5b1cced5e323f4ce72c02d717
ZX <1131484477@qq.com>
2020-08-10 21:11:36 +0200

    location kit  的 push -3

--- app/src/main/java/com/huawei/locationdemo/RequestLocationUpdatesWithCallbackActivity.java
+++ app/src/main/java/com/huawei/locationdemo/RequestLocationUpdatesWithCallbackActivity.java
@@ -51,6 +51,7 @@ public class RequestLocationUpdatesWithCallbackActivity extends BaseActivity imp
 
     private SettingsClient settingsClient;
 //
+    //
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
